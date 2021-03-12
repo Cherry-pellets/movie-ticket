bead2d5a6ace4a011151393b5a2bf68dfc85af06
Cherry=pellets <jinxiu-huang@hotmail.com>
2021-03-12 15:41:55 +0800

    首页-广告管理

---
+++ movie-ticket-admin/src/api/banner.js
@@ -0,0 +1,29 @@
+import axios from '@/utils/axios'
+export const getBanner = (data) => {
+    return axios({
+        url: '/admin/banner/getBanner',
+        method: 'GET',
+        params: data
+    })
+}
+export const updateBannerInfo = (data) => {
+    return axios({
+        url: '/admin/banner/addBanner',
+        method: 'POST',
+        data: data
+    })
+}
+export const upLoadFile = (data) => {
+    return axios({
+        url: '/admin/movie/upLoadFile',
+        method: 'POST',
+        data: data
+    })
+}
+export const deleteBannerInfo = (data) => {
+    return axios({
+        url: '/admin/banner/deleteBanner',
+        method: 'POST',
+        data: data
+    })
+}
