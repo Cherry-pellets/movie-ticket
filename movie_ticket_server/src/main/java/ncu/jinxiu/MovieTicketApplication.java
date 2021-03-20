5995e00dc041eb2d2f3c422a02f5e9684324f95c
Diamond <jinxiu3579@163.com>
2021-03-20 23:45:27 +0800

    添加地址解析配置类

--- movie_ticket_server/src/main/java/ncu/jinxiu/MovieTicketApplication.java
+++ movie_ticket_server/src/main/java/ncu/jinxiu/MovieTicketApplication.java
@@ -1,14 +1,18 @@
 package ncu.jinxiu;
 
+import ncu.jinxiu.config.util.FileProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-//@MapperScan("ncu.jinxiu.mapper")
-//@EnableScheduling
-
+@MapperScan("ncu.jinxiu.mapper")
+@EnableScheduling
+@EnableConfigurationProperties({
+        FileProperties.class
+})
 public class MovieTicketApplication {
     public static void main(String[] args) {
 //        System.setProperty("es.set.netty.runtime.available.processors","false");
