4e4689fd4a1950af93390599cd650d0f633ea5ac
Diamond <jinxiu3579@163.com>
2021-03-26 22:14:43 +0800

    添加shiro及微信token配置类

---
+++ movie_ticket_server/src/main/java/ncu/jinxiu/config/shiro/AdminToken.java
@@ -0,0 +1,10 @@
+package ncu.jinxiu.config.shiro;
+
+import org.apache.shiro.authc.UsernamePasswordToken;
+
+import java.io.Serializable;
+
+public class AdminToken extends UsernamePasswordToken implements Serializable {
+
+
+}
